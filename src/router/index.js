662c44eb56555ba4d3132f470dbfaec2cf0c9224
Minh Tan <tanb2110143@student.ctu.edu.vn>
2024-03-27 22:38:53 +0700

    Tao trang cap nhat lien he

--- src/router/index.js
+++ src/router/index.js
@@ -12,6 +12,12 @@ const routes = [
         name: "notfound",
         component: () => import("@/views/NotFound.vue"),
     },
+    {
+        path: "/contacts/:id",
+        name: "contact.edit",
+        component: () => import("@/views/ContactEdit.vue"),
+        props: true, // Truyền các biến trong $route.params vào làm props
+    },
 ];
 
 const router = createRouter({
